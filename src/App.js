3a3d1b226645f2784400c4bc38533f064c9ca342
Fox <sholtofoxpl@gmail.com>
2019-09-26 13:28:03 -0400

    working tab

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <code>In here for the beginning</code>
         </p>
         <a
           className="App-link"
@@ -18,6 +18,14 @@ function App() {
         >
           Learn React
         </a>
+        <form method="get" title="Search Form" action="https://cse.google.com/cse/publicurl">
+          <div>
+            <input type="text" id="q" name="q" title="Search this site" alt="Search Text" maxlength="256" />
+            <input type="hidden" id="cx" name="cx" value="013626029654558379071:ze3tw4csia4" />
+            <input type="image" id="searchSubmit" name="submit" src="https://www.flaticon.com/free-icon/active-search-symbol_34148" alt="Go" title="Submit Search Query" />
+          </div>
+        </form>
+
       </header>
     </div>
   );
