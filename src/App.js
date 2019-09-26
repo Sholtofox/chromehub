d925d1f0ab153e3538d1459817540808272abe89
Fox <sholtofoxpl@gmail.com>
2019-09-26 13:40:52 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src="https://66.media.tumblr.com/b27ca2192e8881523be0ef9cc7e32727/tumblr_muqinkr95j1s9bsz2o5_250.png"/>
         <p>
-         <code>In here for the beginning</code>
+          <code>In here for the asdasdasbeginning</code>
         </p>
         <a
           className="App-link"
@@ -18,14 +18,36 @@ function App() {
         >
           Learn React
         </a>
-        <form method="get" title="Search Form" action="https://cse.google.com/cse/publicurl">
+        <form
+          method="get"
+          title="Search Form"
+          action="https://cse.google.com/cse/publicurl"
+        >
           <div>
-            <input type="text" id="q" name="q" title="Search this site" alt="Search Text" maxlength="256" />
-            <input type="hidden" id="cx" name="cx" value="013626029654558379071:ze3tw4csia4" />
-            <input type="image" id="searchSubmit" name="submit" src="https://www.flaticon.com/free-icon/active-search-symbol_34148" alt="Go" title="Submit Search Query" />
+            <input
+              type="text"
+              id="q"
+              name="q"
+              title="Search this site"
+              alt="Search Text"
+              maxlength="256"
+            />
+            <input
+              type="hidden"
+              id="cx"
+              name="cx"
+              value="013626029654558379071:ze3tw4csia4"
+            />
+            <input
+              type="image"
+              id="searchSubmit"
+              name="submit"
+              src="https://www.flaticon.com/free-icon/active-search-symbol_34148"
+              alt="Go"
+              title="Submit Search Query"
+            />
           </div>
         </form>
-
       </header>
     </div>
   );
