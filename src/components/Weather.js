f353c42bfad006ee3d8d99ce143398fe76a82e7e
Fox <sholtofoxpl@gmail.com>
2020-01-14 12:05:52 -0500

    updatings

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,43 +1,133 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { getWeatherThunk } from "../store/weather";
+import { getWeatherThunk, getNewWeatherThunk } from "../store/weather";
 
 class Weather extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+      city: "",
+      cityData: {},
+      toggler: false
+    };
+  }
+
+  componentDidMount() {
+    // this.props.cityName ? this.props.getData(this.props.cityName) : this.props.getData('Washington')
+    // this.props.getGeo()
+    this.props.getData(this.props.cityName);
+    this.props.grabShit(this.state.city)
+  }
+  togglerer = () => {
+      this.setState({toggler: !this.state.toggler})
+  }
+
+  toggleEdit = () => {
+    this.setState({ editing: !this.state.editing });
+  };
+
+  onChange = (e) => {
+      this.setState({ city: e.target.value })
+    };
+
+    handleSubmit = async (e) => {
+        try {
+            e.preventDefault()
+
+            await this.props.grabShit(this.state.city)
+            await this.props.weather.newWeather
+        } catch (error) {
+            console.log('err', error)
+        }
+
     }
 
-    componentDidMount() {
-        // this.props.cityName ? this.props.getData(this.props.cityName) : this.props.getData('Washington')
-        // this.props.getGeo()
-        this.props.getData(this.props.cityName)
-    }
+  render() {
+    // console.log("in weather", this.props.news);
+    return (
+      <div className="farts">
+        {this.props.weather.main && this.props.cityName ? (
+          <div className="weatherList">
+            <p>
+              It is {this.props.weather.main.temp}° in {this.props.weather.name}
+            </p>
+            <p>
+              Today, there will be a high of {this.props.weather.main.temp_max}°
+              and a low of {this.props.weather.main.temp_min}°
+            </p>
 
-    render() {
-        console.log('in weather', this.props.news)
-        return (
-            <div className="farts">
-                {this.props.weather.main && this.props.cityName? 
-                <div className='weatherList'>
+            {
+                this.props.weather.newWeather ? (
+                <div>
+                  <p>
+                      It is {this.props.weather.newWeather.main.temp_max} in {this.props.weather.newWeather.name}
+                  </p>
+                <div>
+                <form onSubmit={this.handleSubmit}>
+                  <input
+                    type="text"
+                    name="city"
+                    value={this.state.city}
+                    onChange={this.onChange}
+                  />
+                  <button className="editweather" type="submit">
+                    Add City
+                  </button>
+                </form>
+              </div>
+              </div>
 
-                    <p>It is {this.props.weather.main.temp}° in {this.props.weather.name}</p>
-                    <p>Today, there will be a high of {this.props.weather.main.temp_max}° and a low of {this.props.weather.main.temp_min}°</p>
-                    </div> : <p>loading...</p>
-                }
-            </div>
-        )
-    }
+
+            ) : (
+              <p>
+                No city selected.
+                <button className="editweather" onClick={this.toggleEdit}>
+                  Add City
+                </button>
+              </p>
+            )}
+            {this.state.editing ? (
+              <div>
+                <form onSubmit={this.handleSubmit}>
+                  <input
+                    type="text"
+                    name="city"
+                    value={this.state.city}
+                    onChange={this.onChange}
+                  />
+                  <button className="editweather" type="submit">
+                    Add City
+                  </button>
+                </form>
+
+                <p>Adding City</p>
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </div>
+        ) : (
+          <p>loading...</p>
+        )}
+      </div>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
     getData: (city) => {
         dispatch(getWeatherThunk(city))
+    },
+    grabShit: (newCity) => {
+        dispatch(getNewWeatherThunk(newCity))
     }
 
 })
 
 const mapStateToProps = state => ({
-    weather: state.weather
+    weather: state.weather,
+    newWeather: state.newWeather
 })
 
 const Weathers = connect(mapStateToProps, mapDispatchToProps)(Weather)
--- src/store/weather.js
+++ src/store/weather.js
@@ -4,12 +4,14 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const GET_WEATHER = 'GET_WEATHER'
+const NEW_WEATHER = 'NEW_WEATHER'
 
 
 /**
  * ACTION CREATORS
  */
 const getWeather = weather => ({ type: GET_WEATHER, weather })
+const newWeather = newWeather => ({type: NEW_WEATHER, newWeather})
 
 /**
  * THUNK CREATORS
@@ -28,11 +30,25 @@ export const getWeatherThunk = (city) => {
     }
 }
 
+export const getNewWeatherThunk = (newcity) => {
+    return async dispatch => {
+        try {
+            const { data } = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${newcity}&units=imperial&appid=27933f63744fc9386923b8f3982d9064`
+            );
+            dispatch(newWeather(data))
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+
 /**
  * INITIAL STATE
  */
 const initialState = {
-    weatherData: {}
+    weatherData: {},
+    newWeather: {}
 }
 
 /**
@@ -42,7 +58,9 @@ export default function (state = initialState, action) {
     switch (action.type) {
         case GET_WEATHER:
             return action.weather
-
+        case NEW_WEATHER:
+            state.newWeather = action.newWeather
+            return state
         default:
             return state
     }
